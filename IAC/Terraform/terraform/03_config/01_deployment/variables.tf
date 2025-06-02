f44149fcac525f65423b51559cad1f5f658120d7
277bodnar <ladislav.bodnar@outlook.com>
2025-06-02 14:44:15 +0200

    Update variables.tf

--- IAC/Terraform/terraform/03_config/01_deployment/variables.tf
+++ IAC/Terraform/terraform/03_config/01_deployment/variables.tf
@@ -1,8 +1,3 @@
-variable "location" {
-  description = "The supported azure location where the resource deployed"
-  type        = string
-}
-
 variable "env" {
   description = "The name of the environment to be deployed"
   type        = string
